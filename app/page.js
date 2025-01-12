b6e44111536b8fb9a719e765ed61bc96a5ebdaa4
Asif-102 <asif1zero2@gmail.com>
2025-01-12 16:03:01 +0600

    edit, dete api created

--- app/page.js
+++ app/page.js
@@ -1,8 +1,14 @@
+import AddPin from "./_components/add-pin";
+import ArrangePin from "./_components/arrange-pin";
 import PinList from "./_components/pin-list";
 
 export default function Home() {
   return (
     <section className="m-2">
+      <div className="flex justify-end mx-2">
+        <ArrangePin />
+        <AddPin />
+      </div>
       <PinList />
     </section>
   );
